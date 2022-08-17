627c250234484d55ffe10c13a69bf1ad55be60e6
kaandonmez <kaandonmeztf@gmail.com>
2022-08-17 14:49:41 +0300

    UI Manager'a buton eklemesi yapıldı

--- Assets/_Scripts/Umut/Singletons/UI Manager.cs
+++ Assets/_Scripts/Umut/Singletons/UI Manager.cs
@@ -7,26 +7,34 @@ using TMPro;
 public class UIManager : MonoSingleton<GameManager>
 {
 
-    private int gold = 0;
+    //scene 0 play exit button
+    //scene 1 fight button, 2x buy soldier button + gold indicator(top right)
+    //scene2 = scene1 - no fight button, no buy button
 
 
+    private int gold = 0; //TODO: get gold info from scriptable object
+
+    //TODO!: Ayrı bir scene üzerinde çalışılacak. Umut scene'in den farklı adda scene oluşturulacak.
 
     [SerializeField] private GameObject _startMenuPanel; //scene 0 (play&exit buttons)
     [SerializeField] private Button _playButton; //scene 0 (play&exit buttons)
     [SerializeField] private Button _exitButton; //scene 0 (play&exit buttons)
     
-    [SerializeField] private Gameobject _inGamePanel; //scene 1 (buy panel bottom center)
-    [SerializeFiled] private Button _buyMeleeButton;
-    [SerializeFiled] private Button _buyRangeButton;
+    [SerializeField] private Gameobject _inGamePanel; //scene 1 (buy panel bottom center) //TODO: panelbakgroun için sprite henüz yok drive'a bakılacak. // olmasada olur saydam düzen denenecek.
+    [SerializeField] private Button _buyMeleeButton; //TODO : her iki buton içinde ayrı sprite yapıldı/hazır.
+    [SerializeField] private Button _buyRangeButton; //TODO : her iki buton içinde ayrı sprite yapıldı/hazır.
+    [SerializeField] private Button _fightButton; //scene 1 (top right button to go scene 1.5)
+    [SerializeField] private Button _exitMainMenuButton; //scene 1.5 (top right button to go scene 1 to re-enable buy soldier panel etc.)
     
-    [SerializeField] private Gameobject _goldIndicatorPanel; //scene 1 (top right corner gold indicator)
+    [SerializeField] private Gameobject _goldIndicatorPanel; //scene 1 (top right corner gold indicator) //TODO: put gold sprite in this panel
     [SerializeField] private TextMeshProUGUI _goldIndicatorText; //scene 1 (top right corner gold indicator text)
 
     // Start is called before the first frame update
     void Start()
     {
-
-        
+        _startMenuPanel.SetActive(true);
+        _inGamePanel.SetActive(false);
+        _goldIndicatorPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -37,6 +45,7 @@ public class UIManager : MonoSingleton<GameManager>
 
     public void ShowInGamePanel(){
         _inGamePanel.setActive(true);
+        _goldIndicatorPanel.SetActive(true);
     }
 
     public void HideInGamePanel(){
@@ -44,11 +53,16 @@ public class UIManager : MonoSingleton<GameManager>
     }
 
     public void MainPlayButton(){
-        //TODO: load scene 1 and activate _inGamePanel
+        //TODO: load scene 1 and activate _inGamePanel (done)
+
+        //load scene 1
+        SceneManager.LoadScene(1);
+        //activate _inGamePanel
+        ShowInGamePanel();
     }
 
     public void MainExitButton(){
-        //TODO: exit application
+        //TODO: exit application (done)
         application.Quit();
     }
 
@@ -59,6 +73,17 @@ public class UIManager : MonoSingleton<GameManager>
     public void BuyRangeButton(){
         //TODO: run instantiateRange() method from InstantiateOnGrid.cs script
     }
+
+    public void FightButton(){
+        HideInGamePanel();
+        //TODO!: disable grid in fight scene
+    }
+
+    public void ExitMainMenuButton(){
+        SceneManager.LoadScene(0);
+    }
+
+    //TODO: şimdilik scene1'den scene0'a dönülmüyor. scene1'den scene0'a dönerken soldier pozisyonları ve gold miktarı test edilecek.
 
     public void ShowGoldIndicator(){
         _goldIndicatorPanel.setActive(true);
